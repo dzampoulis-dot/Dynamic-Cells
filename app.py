e35ccf74d43cbe7e8bb6127ec4f7dd5449bb77d6
dzampoulis-dot <dzampoulis@gmail.com>
2026-05-29 18:49:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from flask import Flask
 import os
-import psycopg2
-from psycopg2.extras import DictCursor
+from flask import Flask
 
 app = Flask(__name__)
 
-# Το URL της βάσης σου
-DATABASE_URL = "postgresql://postgres:DynamicCells1!@db.lwxbuotfkpdlqvsuslkx.supabase.co:6543/postgres"
-
 @app.route('/')
 def home():
-    try:
-        # Δοκιμάζουμε μια απλή σύνδεση
-        conn = psycopg2.connect(DATABASE_URL, cursor_factory=DictCursor)
-        conn.close()
-        return "Η εφαρμογή είναι LIVE και συνδεδεμένη με τη βάση!"
-    except Exception as e:
-        return f"Η εφαρμογή είναι LIVE, αλλά η βάση έχει σφάλμα: {str(e)}"
+    return "Η εφαρμογή ξεκίνησε επιτυχώς!"
 
 if __name__ == '__main__':
+    # Το Render θέλει να χρησιμοποιήσει τη δική του θύρα, 
+    # αν δεν υπάρχει, χρησιμοποιούμε την 10000.
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
